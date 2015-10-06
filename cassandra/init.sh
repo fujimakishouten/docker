98c0b3b6f59b03d6b5d55652a39f46721d3e749b
Kazuma Fujimaki <fujimaki@eq-inc.jp>
2015-10-06 11:25:03 +0900

    Add dockerfile for cassandra

---
+++ cassandra/init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+
+
+
+CASSANDRA_HOST_NAME=`hostname -i`
+
+sed -i -e "s/listen_address: localhost/listen_address: $CASSANDRA_HOST_NAME/" /etc/cassandra/cassandra.yaml
+sed -i -e "s/rpc_address: localhost/rpc_address: 0.0.0.0/" /etc/cassandra/cassandra.yaml
+sed -i -e "s/# broadcast_rpc_address: 1\.2\.3\.4/broadcast_rpc_address: $CASSANDRA_HOST_NAME/" /etc/cassandra/cassandra.yaml
+sed -i -e "s/- seeds: \"127\.0\.0\.1\"/- seeds: \"$CASSANDRA_HOST_NAME\"/" /etc/cassandra/cassandra.yaml
+
+/usr/sbin/cassandra -f
+
+
+
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# c-hanging-comment-ender-p: nil
+# End:
