0ed1a6f37b4b035bb927cdae71a2e3e8db2514d2
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2016-09-15 12:55:03 +0900

    Add cassandra container

--- cassandra/init.sh
+++ cassandra/init.sh
@@ -7,12 +7,15 @@
 
 CASSANDRA_HOST_NAME=`hostname -i`
 
+sed -i -e "s/#MAX_HEAP_SIZE=\"4G\"/MAX_HEAP_SIZE=\"256M\"" /etc/cassandra/cassandra-env.sh
+sed -i -e "s/#HEAP_NEWSIZE=\"800M\"/#HEAP_NEWSIZE=\"32M\"" /etc/cassandra/cassandra-env.sh
 sed -i -e "s/listen_address: localhost/listen_address: $CASSANDRA_HOST_NAME/" /etc/cassandra/cassandra.yaml
 sed -i -e "s/rpc_address: localhost/rpc_address: 0.0.0.0/" /etc/cassandra/cassandra.yaml
 sed -i -e "s/# broadcast_rpc_address: 1\.2\.3\.4/broadcast_rpc_address: $CASSANDRA_HOST_NAME/" /etc/cassandra/cassandra.yaml
 sed -i -e "s/- seeds: \"127\.0\.0\.1\"/- seeds: \"$CASSANDRA_HOST_NAME\"/" /etc/cassandra/cassandra.yaml
 
-/usr/sbin/cassandra -f
+
+/usr/sbin/cassandra -R -f
 
 
 
@@ -21,3 +24,4 @@ sed -i -e "s/- seeds: \"127\.0\.0\.1\"/- seeds: \"$CASSANDRA_HOST_NAME\"/" /etc/
 # c-basic-offset: 4
 # c-hanging-comment-ender-p: nil
 # End:
+
